d647d5cfdb671fd8a10dbb34d5f7063e04ad0a38
Oleg Kireev <okireev@ubic.tech>
2022-02-03 18:46:33 +0300

    @/screens/note: create

---
+++ src/screens/note.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function NoteScreen() {
+  return (
+    <View style={{ padding: 10 }}>
+      <Text>This is a note!</Text>
+    </View>
+  );
+}
+export default NoteScreen;
