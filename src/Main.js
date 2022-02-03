3deb2c3fef50a1b8abe1885383a16301895ea4a9
Oleg Kireev <okireev@ubic.tech>
2022-02-03 10:09:20 +0300

    app.json: update app icon paths

--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 const Main = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello world!</Text>
+      <Text>Hello React-native!</Text>
     </View>
   );
 };
