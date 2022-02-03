feebb4ab82449a5d63e507cf0492527022454103
Oleg Kireev <okireev@ubic.tech>
2022-02-03 20:29:53 +0300

    @/components/NoteFeed: create

---
+++ src/components/NoteFeed/index.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { FlatList, View, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const mockNotes = [
+  { id: 1, content: 'Tomorrow Is The Question' },
+  { id: 2, content: 'Tonight At Noon' },
+  { id: 3, content: 'Out To Lunch' },
+  { id: 4, content: 'Green Street' },
+  { id: 5, content: 'In A Silent Way' },
+  { id: 6, content: 'Lanquidity' },
+  { id: 7, content: 'Nuff Said' },
+  { id: 8, content: 'Nova' },
+  { id: 9, content: 'The Awakening' },
+  { id: 10, content: 'Giant Steps' },
+  { id: 11, content: 'Tomorrow Is The Question' },
+  { id: 12, content: 'Tonight At Noon' },
+  { id: 13, content: 'Out To Lunch' },
+  { id: 14, content: 'Green Street' },
+  { id: 15, content: 'In A Silent Way' },
+  { id: 16, content: 'Lanquidity' },
+  { id: 17, content: 'Nuff Said' },
+  { id: 18, content: 'Nova' },
+  { id: 19, content: 'The Awakening' },
+  { id: 20, content: 'Giant Steps' },
+  { id: 21, content: 'Tomorrow Is The Question' },
+  { id: 22, content: 'Tonight At Noon' },
+  { id: 23, content: 'Out To Lunch' },
+  { id: 24, content: 'Green Street' },
+  { id: 25, content: 'In A Silent Way' },
+  { id: 26, content: 'Lanquidity' },
+  { id: 27, content: 'Nuff Said' },
+  { id: 28, content: 'Nova' },
+  { id: 29, content: 'The Awakening' },
+  { id: 30, content: 'Giant Steps' },
+  { id: 31, content: 'Tomorrow Is The Question' },
+  { id: 32, content: 'Tonight At Noon' },
+  { id: 33, content: 'Out To Lunch' },
+  { id: 34, content: 'Green Street' },
+  { id: 35, content: 'In A Silent Way' },
+  { id: 36, content: 'Lanquidity' },
+  { id: 37, content: 'Nuff Said' },
+  { id: 38, content: 'Nova' },
+  { id: 39, content: 'The Awakening' },
+  { id: 40, content: 'Giant Steps' },
+];
+
+const Note = styled.View`
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+  border-radius: 8px;
+  min-height: 100px;
+  width: 95%;
+  padding: 16px;
+  margin: 0 auto 10px;
+`;
+
+function NoteFeed() {
+  return (
+    <View>
+      <FlatList
+        data={mockNotes}
+        keyExtractor={({ id }) => String(id)}
+        renderItem={({ item }) => (
+          <Note>
+            <Text>{item.content}</Text>
+          </Note>
+        )}
+      />
+    </View>
+  );
+}
+
+export default NoteFeed;
