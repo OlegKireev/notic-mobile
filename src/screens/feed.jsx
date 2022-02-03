a8c3edb2edae2d603f2acbfac88ef2662bd7bb71
Oleg Kireev <okireev@ubic.tech>
2022-02-03 19:07:45 +0300

    @/screens/feed: add button to new stack layer

--- src/screens/feed.jsx
+++ src/screens/feed.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import { Text, View, Button } from 'react-native';
 
-function Feed() {
+const propTypes = {
+  navigation: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+function Feed({ navigation }) {
+  const { navigate } = navigation;
+
+  const handleButtonPress = () => {
+    navigate('Note');
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Feed</Text>
+      <Button
+        title="Keep reading"
+        color="#213c61"
+        onPress={handleButtonPress}
+      />
     </View>
   );
 }
+
+Feed.propTypes = propTypes;
+
 export default Feed;
