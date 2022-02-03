915a3f81c38acc7583f3e1054c1ff0124ecc7685
Oleg Kireev <okireev@ubic.tech>
2022-02-03 17:28:13 +0300

    @/Main.js: add styles by StyleSheet

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import {
+  Text, View, Image, StyleSheet,
+} from 'react-native';
 import imageSrc from '../assets/images/hello-world.jpg';
 
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#213c61',
+    fontWeight: '700',
+    fontSize: 36,
+  },
+  desc: {
+    alignSelf: 'flex-end',
+  },
+});
 function Main() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
+      style={styles.wrapper}
     >
-      <Text>Hello React-native!</Text>
+      <Text style={styles.title}>Hello React-native!</Text>
       <Image source={imageSrc} />
+      <Text style={styles.desc}>Image description</Text>
     </View>
   );
 }
