c1e0fdf08dc16f2cc20ff20a90222237cbddccdb
Oleg Kireev <okireev@ubic.tech>
2022-02-03 18:20:58 +0300

    @/Main: add Screens

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,33 +1,9 @@
 import React from 'react';
-import {
-  Text, View, Image, StyleSheet,
-} from 'react-native';
-import imageSrc from '../assets/images/hello-world.jpg';
+import Screens from './screens';
 
-const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    color: '#213c61',
-    fontWeight: '700',
-    fontSize: 36,
-  },
-  desc: {
-    alignSelf: 'flex-end',
-  },
-});
 function Main() {
   return (
-    <View
-      style={styles.wrapper}
-    >
-      <Text style={styles.title}>Hello React-native!</Text>
-      <Image source={imageSrc} />
-      <Text style={styles.desc}>Image description</Text>
-    </View>
+    <Screens />
   );
 }
 
