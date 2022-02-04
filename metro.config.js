a1251e179caae48508d74adc250b651323efce28
Oleg Kireev <okireev@ubic.tech>
2022-02-04 16:25:25 +0300

    metro.config.js: update for compatible with @apollo/client

---
+++ metro.config.js
@@ -0,0 +1,11 @@
+const { getDefaultConfig } = require('metro-config');
+
+const { resolver: defaultResolver } = getDefaultConfig.getDefaultValues();
+exports.resolver = {
+  ...defaultResolver,
+  sourceExts: [
+    ...defaultResolver.sourceExts,
+    'cjs',
+    'jsx',
+  ],
+};
