4a4add4f2481209be14392e2980a10612b89114d
Oleg Kireev <okireev@ubic.tech>
2022-02-04 14:32:29 +0300

    @/screens/note: show selected id

--- src/screens/note.jsx
+++ src/screens/note.jsx
@@ -1,10 +1,19 @@
+import { useNavigationState } from '@react-navigation/native';
 import React from 'react';
 import { Text, View } from 'react-native';
 
 function NoteScreen() {
+  const id = useNavigationState((state) => state.routes
+    .find((route) => route.name === 'Note')
+    .params.id);
+
   return (
     <View style={{ padding: 10 }}>
-      <Text>This is a note!</Text>
+      <Text>
+        This is a note! ID:
+        {' '}
+        {id}
+      </Text>
     </View>
   );
 }
