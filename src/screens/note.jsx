a01ad1629625b33a4dbe2038bcdf61626f8c43b1
Oleg Kireev <okireev@ubic.tech>
2022-02-04 17:59:14 +0300

    @/screens/note: fetch particular note data

--- src/screens/note.jsx
+++ src/screens/note.jsx
@@ -1,20 +1,22 @@
-import { useNavigationState } from '@react-navigation/native';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { useNavigationState } from '@react-navigation/native';
+import { useQuery } from '@apollo/client';
+import { GET_NOTE } from '../api/query';
+import Preloader from '../components/Preloader';
+import Error from '../components/Error';
+import Note from '../components/Note';
 
 function NoteScreen() {
   const id = useNavigationState((state) => state.routes
     .find((route) => route.name === 'Note')
     .params.id);
 
+  const { data, loading, error } = useQuery(GET_NOTE, { variables: { id } });
+
+  if (loading) return <Preloader />;
+  if (error) return <Error>{error.message}</Error>;
   return (
-    <View style={{ padding: 10 }}>
-      <Text>
-        This is a note! ID:
-        {' '}
-        {id}
-      </Text>
-    </View>
+    <Note data={data.note} />
   );
 }
 export default NoteScreen;
