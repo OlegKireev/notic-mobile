d36830d15da38c96de5f201302b3ef4f161015ad
Oleg Kireev <okireev@ubic.tech>
2022-02-07 13:43:08 +0300

    @/routes: create

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+export const routes = {
+  login: 'Login',
+  authLoading: 'AuthLoading',
+  feed: 'Feed',
+  note: 'Note',
+  favorites: 'Favorites',
+  myNotes: 'MyNotes',
+  settings: 'Settings',
+};
