33342c256e26529f3694c9f82ad20256a0f907d3
Oleg Kireev <okireev@ubic.tech>
2022-02-07 14:23:25 +0300

    @/components/UserForm: add props to inputs

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View, Text, TextInput, Button,
 } from 'react-native';
@@ -11,12 +11,27 @@ const propTypes = {
 function UserForm({
   onLogin,
 }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={{ flex: 1 }}>
       <Text>Email:</Text>
-      <TextInput />
+      <TextInput
+        value={email}
+        textContentType="emailAddress"
+        autoCompleteType="email"
+        autoFocus
+        autoCapitalize="none"
+        onChangeText={setEmail}
+      />
       <Text>Password:</Text>
-      <TextInput />
+      <TextInput
+        value={password}
+        textContentType="password"
+        secureTextEntry
+        onChangeText={setPassword}
+      />
       <Button title="Login" onPress={onLogin} />
     </View>
   );
