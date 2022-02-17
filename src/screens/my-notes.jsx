644bed25272fba65256c11a7bf15681fbd3ad0d5
Oleg Kireev <okireev@ubic.tech>
2022-02-17 09:25:04 +0300

    @/screens/my-notes: fetch and display my notes data

--- src/screens/my-notes.jsx
+++ src/screens/my-notes.jsx
@@ -1,10 +1,32 @@
+import { useQuery } from '@apollo/client';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { GET_MY_NOTES } from '../api/query';
+import NoteList from '../components/NoteList';
+import Error from '../components/UI/Error';
+import Preloader from '../components/UI/Preloader';
 
 function MyNotes() {
+  const {
+    loading, error, data, refetch,
+  } = useQuery(GET_MY_NOTES, {
+    fetchPolicy: 'network-only',
+  });
+
+  const handleNoteListRefresh = () => {
+    refetch();
+  };
+
+  if (loading) return <Preloader />;
+  if (error) return <Error>{error.message}</Error>;
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>My Notes</Text>
+      <NoteList
+        data={data.me.notes}
+        loading={loading}
+        onNoteListRefresh={handleNoteListRefresh}
+      />
     </View>
   );
 }
