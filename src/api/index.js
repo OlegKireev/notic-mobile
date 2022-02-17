f76594cf7e45fad51a828a409d3a38faf27e5e31
Oleg Kireev <okireev@ubic.tech>
2022-02-17 09:15:03 +0300

    @/api/index: add authLink

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,29 @@
-import { ApolloClient } from '@apollo/client';
+import { ApolloClient, createHttpLink } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
+import * as SecureStore from 'expo-secure-store';
 import apolloCache from './cache';
 import getEnvVars from '../../config';
 
 const { API_URI } = getEnvVars();
 const uri = API_URI;
+const httpLink = createHttpLink({
+  uri,
+  test: 'test',
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await SecureStore.getItemAsync('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token || '',
+    },
+  };
+});
 
 const apolloClient = new ApolloClient({
   uri,
+  link: authLink.concat(httpLink),
   cache: apolloCache,
   connectToDevTools: true,
 });
