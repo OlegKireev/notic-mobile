4997325b18bd0a64ece9e3e37ce323e4237b5571
Oleg Kireev <okireev@ubic.tech>
2022-02-17 09:27:16 +0300

    @/screens/favorites: fetch and display favorite notes data

--- src/screens/favorites.jsx
+++ src/screens/favorites.jsx
@@ -1,10 +1,32 @@
+import { useQuery } from '@apollo/client';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { GET_MY_FAVORITES } from '../api/query';
+import NoteList from '../components/NoteList';
+import Error from '../components/UI/Error';
+import Preloader from '../components/UI/Preloader';
 
 function Favorites() {
+  const {
+    loading, error, data, refetch,
+  } = useQuery(GET_MY_FAVORITES, {
+    fetchPolicy: 'network-only',
+  });
+
+  const handleNoteListRefresh = () => {
+    refetch();
+  };
+
+  if (loading) return <Preloader />;
+  if (error) return <Error>{error.message}</Error>;
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Favorites</Text>
+      <NoteList
+        data={data.me.favoriteNotes}
+        loading={loading}
+        onNoteListRefresh={handleNoteListRefresh}
+      />
     </View>
   );
 }
