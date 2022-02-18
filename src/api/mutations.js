c0dc489f2564290e04c6d8cf8c34e572254904ed
Oleg Kireev <okireev@ubic.tech>
2022-02-18 09:51:02 +0300

    @/api/mutations: add SIGNUP_USER

--- src/api/mutations.js
+++ src/api/mutations.js
@@ -5,3 +5,9 @@ export const SIGNIN_USER = gql`
       signIn(email: $email, password: $password)
     }
 `;
+
+export const SIGNUP_USER = gql`
+    mutation signUp($email: String!, $username: String!, $password: String!) {
+       signUp(email: $email, username: $username, password: $password)
+    }
+`;
